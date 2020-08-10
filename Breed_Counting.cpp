dcc954d3a12f504266a83658e1ce434dd1306b1c
Mehardeep88 <42536877+mehardeep88@users.noreply.github.com>
2020-08-10 07:15:15 -0700

    Add files via upload
    USACO 2015 December Contest, Silver
    Problem 3. Breed Counting
    Based on prefix sum algorithm

---
+++ Breed_Counting.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+main()
+{
+  freopen("bcount.in", "r", stdin);
+  freopen("bcount.out", "w", stdout);
+  int n,q; cin >> n >> q;
+  int arr[100005][4];
+  for (int i=1;i<=n;i++)
+  {
+    arr[i][1]=arr[i-1][1];     //prefsum
+    arr[i][2]=arr[i-1][2];
+    arr[i][3]=arr[i-1][3];
+    int a;
+    cin >> a;
+    arr[i][a]++;
+  }
+  int a,b;
+  for (int i=0;i<q;i++)
+  {
+    cin >> a >> b;
+    cout << arr[b][1]-arr[a-1][1] << " " << arr[b][2]-arr[a-1][2] << " " << arr[b][3]-arr[a-1][3] << "\n";
+  }
+  return 0;
+}
